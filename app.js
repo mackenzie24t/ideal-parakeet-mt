0975130ba250564c1719abfdcc423b88671de32f
Thompson <mthompson33@una.edu>
2024-08-29 13:43:42 -0500

    docs added

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+
+app.set('view engine', 'ejs')
 app.use(express.static('./public/'))
 
 console.log("hello node server!!!");
@@ -8,6 +10,13 @@ app.get('/', function (req, res) {
   // res.send('Hello World from local devbox :)')
   res.sendFile("index.html");
   
+})
+
+app.get('/ejs', (res, req)=>{
+
+  res.render('index', {
+    myServerVariable : "something from server"
+  });
 })
 
 app.listen(5000)
