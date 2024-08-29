2f655d329f650d51dba87e4a3d0cae825b233151
Thompson <mthompson33@una.edu>
2024-08-29 13:05:51 -0500

    committinggit commit -m

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
+app.use(express.static('./public/'))
 
 console.log("hello node server!!!");
 
 app.get('/', function (req, res) {
   // res.send('Hello World from local devbox :)')
-  app.use(express.static('./'))
+  res.sendFile("index.html");
+  
 })
 
 app.listen(5000)
